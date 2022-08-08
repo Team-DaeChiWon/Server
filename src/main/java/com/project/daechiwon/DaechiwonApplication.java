27bbc6fcc4675b691c8d61b52de8eead7c81aded
yongbin <rhdydqls1024@naver.com>
2022-08-08 21:44:55 +0900

    Initial commit

---
+++ src/main/java/com/project/daechiwon/DaechiwonApplication.java
@@ -0,0 +1,13 @@
+package com.project.daechiwon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaechiwonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaechiwonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/daechiwon/DaechiwonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.daechiwon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaechiwonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
