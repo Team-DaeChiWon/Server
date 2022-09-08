0f704d5ed219c1cfc1ee237128c04e95de1d62f5
yongbin <rhdydqls1024@naver.com>
2022-09-08 11:26:16 +0900

    :memo: :: 수정

--- src/main/java/com/project/daechiwon/domain/user/type/UserType.java
+++ src/main/java/com/project/daechiwon/domain/user/type/UserType.java
@@ -1,5 +1,17 @@
 package com.project.daechiwon.domain.user.type;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum UserType {
     NORMAL, TEACHER
+
+    @JsonCreator
+    public static UserType fromTestEnum(String val){
+        for(UserType userType : UserType.values()){
+            if(userType.name().equals(val)){
+                return userType;
+            }
+        }
+        return null;
+    }
 }
